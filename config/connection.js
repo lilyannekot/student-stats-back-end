f4dd8d69f6e7df8734073829ec84f5d0bdf2d43d
lilys tacos <lilyannek@gmail.com>
2022-08-17 11:33:11 -0700

    grabbed starter code

--- config/connection.js
+++
@@ -1,8 +0,0 @@
-const { connect, connection } = require("mongoose");
-
-connect("mongodb://localhost/studentStats", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = connection;
